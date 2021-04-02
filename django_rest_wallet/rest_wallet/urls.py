6359a3d9ef9773e68d8eb418a288c130c31ed995
Pavel Belov <belovpa112358@gmail.com>
2021-04-02 17:54:31 +0300

    Init urls

--- django_rest_wallet/rest_wallet/urls.py
+++ django_rest_wallet/rest_wallet/urls.py
@@ -6,4 +6,6 @@ from .views import *
 urlpatterns = [
     path('wallet/', WalletView.as_view()),
     path('wallet/<int:pk>', WalletView.as_view()),
+    path('wallet/<int:pk/transactions/', TransactionView.as_view()),
+    path('transaction/', TransactionView.as_view()),
 ]
